17652414947523bc1ce8a71c7a4e76754b606b1c
Ledjo Pilua <ledjopilua@unyt.edu.al>
2021-07-30 23:39:46 +0200

    Refactoring: Observer implementation, Twitter API Keys

---
+++ twitter/ObserverImplementation.java
@@ -0,0 +1,25 @@
+package twitter;
+
+import twitter4j.Status;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * ObserverImplementation Class is used for testing Twitter package implementing Observer design
+ */
+
+public class ObserverImplementation implements Observer{
+
+    private int numberOfTweets = 0;
+
+    public int getNTweets() { return numberOfTweets; }
+
+
+    @Override
+        public void update(Observable o, Object arg) {
+            Status s = (Status) arg;
+            numberOfTweets++;
+        }
+
+}
